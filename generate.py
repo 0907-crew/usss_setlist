4ba7e8e38f40d58390e08d011c7d4fd25f0630b9
Anzsnr <ao.niji.ra.0707.1110@gmail.com>
2026-09-07 20:40:15 +0900

    Data update

--- generate.py
+++ generate.py
@@ -176,6 +176,7 @@ html_template = """<!DOCTYPE html>
 <head>
     <meta charset=\"UTF-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <meta name="robots" content="noindex,nofollow">
     <title>現場まとめ App</title>
     <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\">
     <style>
